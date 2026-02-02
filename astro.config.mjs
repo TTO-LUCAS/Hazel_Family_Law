928101141f5e73e7b08f2275138ee342403754ff
priitey <prwhite17@gmail.com>
2026-02-02 21:45:28 +1100

    added compression w gzip and added lazy loading to some img tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import compressor from 'astro-compressor';
+import { gzip } from 'astro-compressor/dist/compress';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://hazel-family-law.netlify.app/'
+  site: 'https://hazel-family-law.netlify.app/',
+  integrations: [compressor( { gzip: true, brotli: false } )],
 });
